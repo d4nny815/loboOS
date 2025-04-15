524d3be00aabc4348f9cd67e983573e75b13b26e
danny <d4nny815@gmail.com>
2025-04-14 22:38:06 -0700

    remove print_tets

--- src/print.c
+++ src/print.c
@@ -167,28 +167,4 @@ void print_long_hex(unsigned long l, bool prefix) {
   print_str(p_buf);
 }
 
-void printk_tests() {
-  printk("%c", 'a'); // should be "a"
-  printk("%c", 'Q'); // should be "Q"
-  printk("%c", 256 + '9'); // Should be "9"
-  printk("\n");
-  printk("%s\n", "test string"); // "test string"
-  printk("foo%sbar\n", "blah"); // "fooblahbar"
-  printk("foo%%sbar\n"); // "foo%bar" // TODO
-  printk("%d\n", 815); // "815"
-  printk("%d\n", INT_MIN); // "-2147483648"
-  printk("%d\n", INT_MAX); // "2147483647"
-  printk("%u\n", 0); // "0"
-  printk("%u\n", UINT_MAX); // "4294967295"
-  printk("%x\n", 0xDEADbeef); // "deadbeef"
-  printk("%p\n", (void*)UINTPTR_MAX); // "0xFFFFFFFFFFFFFFFF"
-  printk("%hd\n", 0x8000); // "-32768"
-  printk("%hd\n", 0x7FFF); // "32767"
-  printk("%hu\n", 0xFFFF); // "65535"
-  printk("%ld\n", LONG_MIN); // "-9223372036854775808"
-  printk("%ld\n", LONG_MAX); // "9223372036854775807"
-  printk("%lu\n", ULONG_MAX); // "18446744073709551615"
-  printk("%qd\n", (long long int)LONG_MIN); // "-9223372036854775808"
-  printk("%qd\n", (long long int)LONG_MAX); // "9223372036854775807"
-  printk("%qu\n", (long long unsigned int)ULONG_MAX); // "18446744073709551615"
-}
+
--- src/print.h
+++ src/print.h
@@ -2,7 +2,6 @@
 #define PRINT_H
 
 int printk(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
-void printk_tests();
 
 
 #endif /* print.h */
